24f288c8f3b1ca7a1b45befcbb4af35d58ae6066
Noah Santorello <nsantorello@gmail.com>
2013-11-07 07:15:04 -0800

    updated license in project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
   				[org.clojure/data.json "0.2.3"]
   				[http-kit "2.1.12"]]
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :repl-options {:init-ns dreamhost-dns.core})
